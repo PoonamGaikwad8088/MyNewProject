3d576ea9adf76a66954f62e735c5a7efa053d04e
PoonamGaikwad8088 <poonam8088@gmail.com>
2018-08-31 09:57:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParentModule } from './parent/parent.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
